2e8aa51eb283dcb331a31a7f554cfee052bbc7ca
evkoff <f.evgenika@gmail.com>
2026-04-09 13:18:30 +0300

    Fix: final folder structure cleanup

--- Lessons/lesson.py
+++ Lessons/lesson.py
@@ -87,18 +87,159 @@ name = "Student"
 # disney_users_C = {name: index for index, name in enumerate(sorted_users)}
 # print(disney_users_C)
 
-items_purchase = {"Water": "$1", "Bread": "$3", "TV": "$1,000", "Fertilizer": "$20"}
-wallet = "$300"
-wallet_value = int(wallet.replace("$", "").replace(",", ""))
-basket = []
-for item, price in items_purchase.items():
-    price_value = int(price.replace("$", "").replace(",", ""))
-    if price_value <= wallet_value:
-        basket.append(item)
-        wallet_value -= price_value
-    else:
-        print(f"You cannot afford {item} which costs {price}.")
-if not basket:
-    print("Nothing")
-else:
-    print(sorted(basket))
+# items_purchase = {"Water": "$1", "Bread": "$3", "TV": "$1,000", "Fertilizer": "$20"}
+# wallet = "$300"
+# wallet_value = int(wallet.replace("$", "").replace(",", ""))
+# basket = []
+# for item, price in items_purchase.items():
+#     price_value = int(price.replace("$", "").replace(",", ""))
+#     if price_value <= wallet_value:
+#         basket.append(item)
+#         wallet_value -= price_value
+#     else:
+#         print(f"You cannot afford {item} which costs {price}.")
+# if not basket:
+#     print("Nothing")
+# else:
+#     print(sorted(basket))
+
+
+
+# def calculation(a, b):
+#     addition = a + b
+#     substraction = a - b
+
+# res = calculation(40, 10)
+# print(res)
+
+# def double(n):
+#   return n * 2
+
+# myList = [10, 25, 17, 9, 30, -5]
+
+# myList2 = map(double, myList)
+# print (list(myList2))
+# The code is longer!
+
+# def multipleOf5(n):
+#   if(n % 5 == 0):
+#     return n
+
+# myList = [10, 25, 17, 9, 30, -5]
+
+# myList2 = filter(multipleOf5, myList)
+# print (list(myList2))
+
+# myList = [10, 25, 17, 9, 30, -5]
+# # Filters the elements which are not multiples of 5
+# myList2 = filter(lambda n : n%5 == 0, myList)
+# print (list(myList2))
+
+# lst = [1, 2, 3, 4, "a", "b", "c", 38.5, "56.7"]
+# print(lst)
+# print(type(lst))
+# print(type(lst[-1])) 
+
+# l1 = [1, 2, 3, 4, 5, 6]
+# l1.append(3)
+# l1.extend([7, 8, 9])
+# l1.extend([6, 7, 8])
+# print(l1)
+
+
+# l1=[1, 2, 3, 4, 5, 6]
+# l1.insert(2, 6)
+# print(l1)
+# a = ["b", "g", "a", "d", "f", "c", "h", "e"]
+# x = sorted(a)
+# print("a after sorted function")
+# print(a)
+# print(x)
+# b = [1, 2, 5, 8, 3]
+# b.sort()
+# print(b)
+
+# lst = [1, 2, 3, 4, 5, 6, 7]
+# print(lst[0:4])
+# print(lst[::])
+# print(lst[::-1])
+
+# lst = [1, 2, 3, 4, 5, 6, 7]
+# print(lst.pop())
+# print(lst)
+# lst.remove(4)
+# print(lst) 
+# lst.clear()
+# print(lst)
+
+# t = (1, 2, 3, 4, 5, "a", "b", "c")
+# t1 = 1, 2, 3, 4, "g", "l"
+# print(t)
+# print(t1)
+# print(len(t))
+
+# t1 = (1, 2, 3, 4, 5)
+# t2 = (6, 7, 8, 9)
+# t3 = t1 + t2 
+# print(t3)
+
+# d = {1:'10', 2:'20', 3:'30', 4:'40', 5:'50'}
+# l1 =list(d.keys())
+# print("the key values are:")
+# print(l1)
+# l2 = list(d.values())
+# print("The values are of dictionary is")
+# print(l2)
+# l3 = list(d.items())
+# print("the list items are")
+# print(l3)
+
+# set1 = {1, 2, 3, 4, "hi", "world", "python"}
+# print("python" in set1)
+# set1.remove(4)
+# print(set1)
+
+# a = {1, 2, 3, 4, 5}
+# b = {2, 3, 6, 7, 5}
+# c = a^b 
+# print(c)
+# d = a - b
+# print(d)
+# e = b - a 
+# print(e)
+
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = first_name + ' ' + last_name
+#     return full_name.title()
+
+# musician = get_formatted_name('jimi', 'hendrix') 
+# print(musician)
+
+# def format_name(first_name, last_name):
+#     return (first_name.title(), last_name.title())
+
+# first, last = format_name("RICk", "mORTY")
+# print(first)
+# print(last)
+
+# def my_function(username, **details):
+#   print("Username:", username)
+#   print("Additional details:")
+#   for key, value in details.items():
+#     print("  ", key + ":", value)
+
+# my_function("emil123", age = 25, city = "Oslo", hobby = "coding")
+
+people = ["Rick", "Morty", "Beth", "Jerry", "Snowball"]
+filtered_names = filter(lambda name: len(name) >= 4, people)
+hello_people = map(lambda name: f"Hello, {name}", filtered_names)
+
+
+print(list(hello_people))
+
+
+
+
+
+
